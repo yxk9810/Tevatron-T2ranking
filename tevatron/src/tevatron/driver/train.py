99a00b30b159bb9547335d1b9870dd208827ca82
JackieWu <wujindou1@163.com>
2024-04-29 07:24:50 +0800

    Update train.py

--- tevatron/src/tevatron/driver/train.py
+++ tevatron/src/tevatron/driver/train.py
@@ -58,7 +58,7 @@ def main():
     logger.info("Training/evaluation parameters %s", training_args)
     logger.info("MODEL parameters %s", model_args)
     torch.distributed.init_process_group(backend='nccl',
-                                     init_method='env://',rank = torch.cuda.device_count())
+                                     init_method='env://',rank = torch.cuda.device_count(),world_size = 1)
     set_seed(training_args.seed)
 
     num_labels = 1
